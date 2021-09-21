ad50d53d70c7f85e1bb93ab6162c498db0995856
zhiwi1 <zhyuliuk.ivan@gmail.com>
2021-09-21 20:04:20 +0300

    Update Utils

--- api/src/main/java/edu/zhyuliuk/task/Utils.java
+++ api/src/main/java/edu/zhyuliuk/task/Utils.java
@@ -4,7 +4,11 @@ import by.zhyuliuk.task.utils.StringUtils;
 
 import java.util.Arrays;
 
-public class Utils {
+public final class Utils {
+    private Utils() throws IllegalAccessException {
+        throw new IllegalAccessException();
+    }
+
     public static boolean isAllPositiveNumbers(String... str) {
         return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
--- core/src/main/java/edu/zhyuliuk/task/App.java
+++ core/src/main/java/edu/zhyuliuk/task/App.java
@@ -2,6 +2,6 @@ package edu.zhyuliuk.task;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println( Utils.isAllPositiveNumbers("12", "79"));
+        System.out.println( Utils.isAllPositiveNumbers("12", "7s9"));
     }
 }
